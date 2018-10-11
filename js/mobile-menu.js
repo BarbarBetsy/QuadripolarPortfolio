240eb1b1beef60f3a4e39ecf4f0b301bed3ea015
Julia Cam <barbarbetsy@googlemail.com>
2018-10-11 10:14:40 +0200

    fail

--- js/mobile-menu.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $(".fa-bars").on("click", function() {
-    $(".menu").toggleClass("open");
-  });
-
-  $(".menu").on("click", function() {
-    $(".menu").toggleClass("open");
-  });  
-});
